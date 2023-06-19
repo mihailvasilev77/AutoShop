cfee4a2cbd04036556418dde27e5a798bd66af89
mishopazarlucka <mihailbg2005@gmail.com>
2023-06-19 17:51:22 +0300

    Create Suspension.java

---
+++ src/Suspension.java
@@ -0,0 +1,29 @@
+public class Suspension extends Item {
+    private int size;
+    private int diamater;
+
+
+    public Suspension(int id, String name, int price, int size, int diamater) {
+        super(id,name,price);
+        this.size = size;
+        this.diamater = diamater;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getDiamater() {
+        return this.diamater;
+    }
+
+    public void setDiamater(int diamater) {
+        this.diamater = diamater;
+    }
+
+
+}
