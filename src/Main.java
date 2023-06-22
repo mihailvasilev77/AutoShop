67718d4cd77176de1d09a00b49b9aef0b9349d7a
HristoAngeloff <66922355+Iccooo@users.noreply.github.com>
2023-06-22 14:38:40 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,57 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         AutoShop shop = new AutoShop();
-        Breaks br = new Breaks("spi", 4, "rachki", 12);
-        Suspension sus = new Suspension("Suspenison", 0, 0, 0);
-        Wheel wh = new Wheel("name",1,1, 32);
+        
+        Breaks br_test = new Breaks("spi", 4, "rachki", 12);
+        Suspension sus_test = new Suspension("Suspenison", 0, 0, 0);
+        Wheel wh_test = new Wheel("name",1,1, 32);
 
-        shop.addPart(wh);
-        shop.addPart(sus);
-        shop.addPart(br);
-
-        shop.displayAllParts();
-        shop.sortPartsByPrice();
-        shop.displayAllParts();
-        shop.sortPartsByID();
-        shop.displayAllParts();
+        shop.addPart(wh_test);
+        shop.addPart(sus_test);
+        shop.addPart(br_test);
 
 
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("Welcome to the our Autoshop");
+            System.out.println("1. Sort by ID and display items");
+            System.out.println("2. Sort by price and display items");
+            System.out.println("3. Add item");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Sorting by ID");
+                        shop.sortPartsByID();
+                        shop.displayAllParts();
+                    break;
+                case 2:
+                    System.out.println("Sorting by price");
+                        shop.sortPartsByPrice();
+                        shop.displayAllParts();
+                    break;
+                case 3:
+                    System.out.println("Add item");
+                    // to do 
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+            System.out.println();
+        } while (choice != 4);
+
+        scanner.close();
     }
+
+
+
 }
